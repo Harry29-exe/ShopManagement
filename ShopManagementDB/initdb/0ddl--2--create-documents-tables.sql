d0e5821f593e03b74975691fd267a4801f092ade
Kamil Wójcik <wojcikakamil@gmail.com>
2022-04-15 23:06:26 +0200

    added new tables to database model, split table creation into 2 files

--- ShopManagementDB/initdb/0ddl--2--create-documents-tables.sql
+++ ShopManagementDB/initdb/0ddl--2--create-documents-tables.sql
@@ -1,36 +1,3 @@
-CREATE TABLE users
-(
-    id            SERIAL8 PRIMARY KEY NOT NULL,
-    username      varchar(32) UNIQUE  NOT NULL,
-    password_hash varchar(80)         NOT NULL,
-    name          varchar(32)         NOT NULL,
-    surname       varchar(48)         NOT NULL
-);
-
-CREATE TABLE business_entities
-(
-    id           SERIAL8 PRIMARY KEY NOT NULL,
-    name         varchar(256)        NOT NULL,
-    country_code char(2)             NOT NULL,
-    city         varchar(256)        NOT NULL,
-    nip          varchar(128)        NOT NULL,
-    phone_number varchar(28),
-    email        varchar(128)
-);
-
-CREATE TABLE items
-(
-    id                SERIAL8 PRIMARY KEY NOT NULL,
-    code_name         varchar(64)         NOT NULL,
-    name              varchar(192)        NOT NULL,
-    type              int2                NOT NULL, -- enum
-    price             decimal             NOT NULL,
-    tax_rate          DECIMAL             NOT NULL,
-    description       text                NOT NULL,
-    quantity_in_stock int8                NOT NULL
-
-);
-
 CREATE TABLE purchase_invoices
 (
     id            SERIAL8 PRIMARY KEY NOT NULL,
